3a5e5a52ef930b62f2e233dd020683ae0c792e30
Obert Gwamure <obertgwamure@gmail.com>
2023-09-18 09:51:00 -0400

    adding Lead and Agent model to Leads app

--- leads/models.py
+++ leads/models.py
@@ -2,17 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('Yt', 'Youtube'),
-        ('Gg', 'Google'),
-        ('Fb', 'Facebook'),
-    )
-
+    
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     age = models.IntegerField(default=0)
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=20, default="Yt")
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
