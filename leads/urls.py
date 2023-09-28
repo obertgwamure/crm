13fa4407b7a5caba8c7724bfa210bbaa78880bb8
Obert Gwamure <obertgwamure@gmail.com>
2023-09-28 15:42:16 -0400

    added lead_detailed view and template

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import leads_list
+from .views import lead_list, lead_detail
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('',leads_list, name='lead_list'),
+    path('', lead_list, name='leads_list'),
+    path('<pk>/', lead_detail, name='lead_detail'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Lead
 
-def leads_list(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         'leads': leads
     }
-    return render(request, 'leads/leads_list.html', context)
+    return render(request, 'leads/lead_list.html', context)
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    context = {
+        'lead': lead,
+    }
+    return render(request, 'leads/lead_detail.html', context)
