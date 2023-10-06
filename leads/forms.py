0df3a4d23c121a57f901812175a64bd8453e2ca3
Obert Gwamure <obertgwamure@gmail.com>
2023-10-06 06:05:42 -0400

    added srylling with tail wind css and redid the templates to work with the tailwind css components

--- leads/forms.py
+++ leads/forms.py
@@ -11,7 +11,3 @@ class LeadModelForm(forms.ModelForm):
             'agent',
         )
 
-class LeadForm(forms.Form):
-    first_name = forms.CharField(max_length=25)
-    last_name = forms.CharField(max_length=25)
-    age = forms.IntegerField(min_value=18)
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Lead, Agent
+from .models import Lead
 from .forms import LeadModelForm
+
+def landing_page(request):
+    return render (request,'landing_page.html')
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -24,19 +26,7 @@ def lead_create(request):
     if request.method == "POST":
         form = LeadModelForm(request.POST)
         if form.is_valid():
-            form = form.cleaned_data
-            first_name = form['first_name']
-            last_name = form['last_name']
-            age = form['age']
-            agent = form['agent']
-
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent
-            )
-
+            form.save()
             return redirect('/leads')
         else:
             print('error creating form')
@@ -45,3 +35,31 @@ def lead_create(request):
         'form': form
     }
     return render(request,'leads/lead_create.html', context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/leads/{pk}')
+        else:
+            print('error creating form')
+    
+    context = {
+        'lead': lead,
+        'form': form
+    }
+    return render(request,'leads/lead_update.html', context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    if request.method == 'POST':
+        lead.delete()
+        return redirect('/leads')
+    context = {
+        'lead': lead
+    }
+    return render(request, 'leads/lead_delete.html',context)
